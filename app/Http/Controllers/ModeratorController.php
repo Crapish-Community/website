31489f92ab656e37ef3085b904ea09326ce5d514
CozmoDev <mileaalexandru7@gmail.com>
2023-09-03 15:18:53 +0300

    forgot to add sumthin

--- app/Http/Controllers/ModeratorController.php
+++ app/Http/Controllers/ModeratorController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Http;
+use App\Models\User;
+use App\Models\Ban;
 
 class ModeratorController extends Controller
 {

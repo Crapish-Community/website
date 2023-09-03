41bb2c1d7fade3a106d5ec296355d85db91aa330
CozmoDev <mileaalexandru7@gmail.com>
2023-09-03 16:40:46 +0300

    i forgot a controller

--- app/Http/Controllers/ModeratorController.php
+++ app/Http/Controllers/ModeratorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 use App\Models\User;
 use App\Models\AdminLog;
 use App\Models\InviteKey;

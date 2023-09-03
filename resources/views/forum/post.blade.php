a8ad5e7d806bd887f55759d2a0cc5b7e67e1051f
CarlTheObeseCat <51877146+CarlTheObeseCat@users.noreply.github.com>
2023-09-03 15:40:24 +0100

    Update post.blade.php

--- resources/views/forum/post.blade.php
+++ resources/views/forum/post.blade.php
@@ -130,7 +130,7 @@
                                     </div>
                                     <br>
                                     <img class="img-fluid" style="max-height: 128px;" data-tadah-thumbnail-id="{{ $reply->user->id }}" data-tadah-thumbnail-type="user-thumbnail" src="{{ asset('images/thumbnail/blank.png') }}" alt="{{ $reply->user->username }}">
-                                    <p class="m-0 d-inline-block"><a @if ($reply->user->isAdmin()) class="font-weight-bold text-danger" @endif @if ($reply->user->isModerator()) class="font-weight-bold text-success" @endif href="{{ route('users.profile', $post->user->id) }}">{{ $post->user->username }}</a></p>
+                                    <p class="m-0">@if ($reply->user->isAdmin()) <p class="text-danger mt-0 mb-0 font-weight-bold"><i class="fas fa-shield mr-1"></i>Administrator</p> @endif @if ($reply->user->isModerator()) <p class="text-success mt-0 mb-0 font-weight-bold"><i class="fas fa-user-shield mr-1"></i>Moderator</p> @endif Joined: <p class="text-muted d-inline">{{ date('m/d/Y', strtotime($post->user->joined)) }}</p><br>Posts: <p class="text-muted d-inline">{{ $post->user->countPosts() }}</p></p>
                                 </div>
                                 <div class="col-md-10">
                                     <p class="text-muted mb-0"><small>Posted on {{ date('F j, Y, g:i A', strtotime($reply->created_at)) }} ({{ $reply->created_at->diffForHumans() }})</small></p>
